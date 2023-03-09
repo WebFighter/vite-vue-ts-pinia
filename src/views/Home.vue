743878ffed5a612145c8d6ea7d34e1dfc23f7ef3
px zhou <zhoupuxian@well-healthcare.com>
2023-03-09 16:00:00 +0800

    测试小功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,53 @@
 <template>
     <div>
-        {{ store.count }}
-        <el-button @click="store.increment">加一</el-button>
-        <el-button @click="store.doubleCount">乘二</el-button>
+        <br>
+        <!-- {{ store.count }} -->
+        <!-- <el-button @click="store.increment">加一</el-button> -->
+        <!-- <el-button @click="store.doubleCount">乘二</el-button> -->
+        <el-input style="width: 250px;" v-model="searchValue"></el-input>
+        <el-button @click="search">搜索</el-button>
+        <h3 v-html="paragraph"></h3>
     </div>
 </template>
 
 <script lang='ts' setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 // import { userService } from '../config/api';
 // import { userUrl } from '../config/url';
-import { useCounterStoreForSetup } from '../store';
-const store=useCounterStoreForSetup();
-const state = reactive({
-    userName: ''
-})
+// import { useCounterStoreForSetup } from '../store';
+// const store=useCounterStoreForSetup();
+let searchValue=ref<string>('')
+let defaultParagraph=`我曾经看过山和大海，陈才敏大sb，陈才敏陈才敏大sb，陈才敏大sb，哈哈哈，一二三四五，上山打老虎`
+let paragraph=ref<string>(defaultParagraph);
+const search=()=>{
+    let value=searchValue.value;
+    if(value.length>defaultParagraph.length)return;
+    let len=value.length;
+    let l=0;
+    let res=''
+    while(l<defaultParagraph.length-len){
+        let str=equalStr(l,len);
+        if(str==value){
+            res+=getStyleSearch(str);
+            l+=len
+        }else{
+            res+=defaultParagraph[l];
+            l++
+        }
+    }
+    paragraph.value=res
+}
+
+const equalStr=(i:number,len:number)=>{
+    let str='';
+    for(;;i++){
+        str+=defaultParagraph[i];
+        if(str.length==len)return str
+    }
+}
+const getStyleSearch=(str:string)=>{
+    return `<span style="background-color: #FDCC00;">${str}</span>`
+}
 
 // getUser()
 
@@ -27,5 +60,4 @@ const state = reactive({
 </script>
 
 <style scoped>
-
 </style>
